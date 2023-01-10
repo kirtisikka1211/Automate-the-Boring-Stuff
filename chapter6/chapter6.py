7e788bef3d89cfbc85d416ae95acc971e785081d
kirtisikka1211 <kirtisikka972@gmail.com>
2023-01-10 18:40:58 +0530

    Automate the

---
+++ chapter6/chapter6.py
@@ -0,0 +1,18 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+def printable(n):
+    colWidths = [0] * len(n)
+    for x in range(len(n)):
+        for y in n[x]:
+            if  colWidths[x]<len(y):
+                colWidths[x]=len(y)
+    for y in range(len(n[0])):
+        for x in range(len(n)):#rotate ker rahe
+            print(n[x][y].rjust(colWidths[x]),end=' ')
+        print()
+        
+printable(tableData)
+            
+    
+    
