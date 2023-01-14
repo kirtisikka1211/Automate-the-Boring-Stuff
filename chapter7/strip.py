0be3a1515124ee82e8374b3dee1fb273d18bde82
kirtisikka1211 <kirtisikka972@gmail.com>
2023-01-15 01:57:15 +0530

    updating chapter 7

--- chapter7/strip.py
+++ chapter7/strip.py
@@ -1,15 +1,15 @@
 import re
 def white(string, c):
     if c != '':
-        strip = re.compile(c)
-        newstring = strip.sub('', string)
-        return newstring
-    else:
-        strip = re.compile('^\s*')
-        newstring = strip.sub('', string)
-        strip= re.compile('\s*$')
-        newstring = strip.sub('', newstring)
-        return newstring
+    #     strip = re.compile(c)
+    #     newstring = strip.sub('', string)
+    #     return newstring
+    # else:
+    #     strip = re.compile('^\s*')
+    #     newstring = strip.sub('', string)
+    #     strip= re.compile('\s*$')
+    #     newstring = strip.sub('', newstring)
+    #     return newstring
 test=input("enter the text")
 ch="Enter thenj"
 newstring = white(test, ch)
