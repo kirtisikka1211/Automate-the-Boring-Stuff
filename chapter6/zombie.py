af7838fded168123c186898419b7ed7feac63fd5
kirtisikka1211 <kirtisikka972@gmail.com>
2023-01-15 16:27:13 +0530

    updating chapter6,chapter5,7

--- chapter6/zombie.py
+++ chapter6/zombie.py
@@ -1,4 +1,5 @@
 import zombiedice
+import random
 
 class MyZombie:
     def __init__(self, name):
@@ -6,6 +7,7 @@ class MyZombie:
         self.name = name
 
     def turn(self, gameState):
+  
         # gameState is a dict with info about the current state of the game.
         # You can choose to ignore it in your code.
 
@@ -28,15 +30,84 @@ class MyZombie:
                 diceRollResults = zombiedice.roll() # roll again
             else:
                 break
+            
+            
+            
+            
+            
+class MyZombieShotgun2:
+    def __init__(self, name):
+        # All zombies must have a name:
+        self.name = name
+
+    def turn(self, gameState): 
+        diceRollResults = zombiedice.roll()          
+        shotGun = 0
+        while diceRollResults is not None:
+            shotGun += diceRollResults['shotgun']
+
+            if shotGun < 2:
+                diceRollResults = zombiedice.roll() # roll again
+            else:
+                break
+class MyZombieRandom1:
+    def __init__(self, name):
+        # All zombies must have a name:
+        self.name = name
+
+    def turn(self, gameState): 
+        diceRollResults = zombiedice.roll()          
+        shotGun = 0
+        while diceRollResults and random.randint(0,1)==0:
+            diceRollResults = zombiedice.roll()   
+class MyZombie1to4ShotGun:
+    def __init__(self, name):
+        # All zombies must have a name:
+        self.name = name
+
+    def turn(self, gameState): 
+        runRec=4;
+        diceRollResults = zombiedice.roll()          
+        shotGun = 0
+        while diceRollResults is not None and runRec>0:
+            shotGun += diceRollResults['shotgun']
+            runRec -=1
+
+            if shotGun < 2:
+                diceRollResults = zombiedice.roll() # roll again
+            else:
+                break
+class MyZombieMoreShotGunThanBrains:
+    def __init__(self, name):
+        # All zombies must have a name:
+        self.name = name
+
+    def turn(self, gameState): 
+       diceRollResults = zombiedice.roll()          
+       shotGun = 0
+       brains=0
+       while diceRollResults is not None:
+            shotGun += diceRollResults['shotgun']
+            brains += diceRollResults['brains']
+
+            if shotGun < brains:
+                diceRollResults = zombiedice.roll() # roll again
+            else:
+                break
+            
+          
 
 zombies = (
     zombiedice.examples.RandomCoinFlipZombie(name='Random'),
     zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2
-Shotguns', minShotguns=2),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1
-Shotgun', minShotguns=1),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
     MyZombie(name='My Zombie Bot'),
+    MyZombieShotgun2(name="Shotgunpro"),
+    MyZombieRandom1(name="kirti"),
+    MyZombie1to4ShotGun(name="naman"),
+    MyZombieMoreShotGunThanBrains(name="Moreshotguns")
+    
     # Add any other zombie players here.
 )
 
--- chapter7/chapter7.py
+++ chapter7/chapter7.py
@@ -1,1 +1,28 @@
 import re
+
+def datedetection(text):
+  leap_Year = False
+  month_Days ={'01':'31','02':'28','03':'31','04':'30','05':'31','06':'30','07':'31','08':'31','09':'30','10':'31','11':'30','12':'31'}
+  
+  date = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{1,4})')
+
+  result = (date.findall(text))
+  for i in range(len(result)):
+    day, month, year = result[i]
+
+    if month not in month_Days.keys(): 
+      return 'Invalid month'
+    if int(year) % 4 == 0: 
+      leap_Year = True
+      if int(year) % 100 == 0 and int(year) % 400 != 0:
+        leapYear = False
+
+    if leap_Year:
+      month_Days['02'] = '29'
+    if month_Days[month] < day: 
+      return 'Invalid date'
+
+  return 'Valid Date'    
+x=input("Enter the date u want to check in format dd/mm/yy")
+
+print(datedetection(x))
